ca3d0d308f43dda756a13e1ae559e73ea8ed5998
minwooseo <alsdn1211@naver.com>
2024-08-08 10:42:52 +0900

    base selfhealingweb

---
+++ api/v1/selfhealingweb_types.go
@@ -0,0 +1,77 @@
+/*
+Copyright 2024.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+type PodStatus struct {
+	PodName string `json:"podName,omitempty"`
+	PodStatus string `json:"podStatus,omitempty"`
+	PodStatusCode int `json:"podStatusCode,omitempty"`
+}
+
+type WatcherStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+	PodStatus []PodStatus `json:"podStatus,omitempty"`
+}
+
+// SelfhealingWebSpec defines the desired state of SelfhealingWeb
+type SelfhealingWebSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of SelfhealingWeb. Edit selfhealingweb_types.go to remove/update
+	Replicas int32 `json:"replicas"`
+}
+
+// SelfhealingWebStatus defines the observed state of SelfhealingWeb
+type SelfhealingWebStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+	HealthStatus string `json:"healthStatus"`
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+
+// SelfhealingWeb is the Schema for the selfhealingwebs API
+type SelfhealingWeb struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   SelfhealingWebSpec   `json:"spec,omitempty"`
+	Status SelfhealingWebStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// SelfhealingWebList contains a list of SelfhealingWeb
+type SelfhealingWebList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []SelfhealingWeb `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&SelfhealingWeb{}, &SelfhealingWebList{})
+}
---
+++ internal/controller/selfhealingweb_controller.go
@@ -0,0 +1,126 @@
+/*
+Copyright 2024.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controller
+
+import (
+	"context"
+
+	"k8s.io/apimachinery/pkg/runtime"
+
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+
+	ctrl "sigs.k8s.io/controller-runtime"
+
+	appv1 "web.test/asuka/api/v1"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"	
+)
+
+// SelfhealingWebReconciler reconciles a SelfhealingWeb object
+type SelfhealingWebReconciler struct {
+	client.Client
+	Scheme *runtime.Scheme
+}
+
+// +kubebuilder:rbac:groups=app.web.test,resources=selfhealingwebs,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=app.web.test,resources=selfhealingwebs/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=app.web.test,resources=selfhealingwebs/finalizers,verbs=update
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+// TODO(user): Modify the Reconcile function to compare the state specified by
+// the SelfhealingWeb object against the actual cluster state, and then
+// perform operations to make the cluster state reflect the state specified by
+// the user.
+//
+// For more details, check Reconcile and its Result here:
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.18.4/pkg/reconcile
+func (r *SelfhealingWebReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	log := log.FromContext(ctx)
+	
+	var selfhealingWeb appv1.SelfhealingWeb
+	if err := r.Get(ctx, req.NamespacedName, &selfhealingWeb); err != nil {
+		log.Error(err, "Error Getting SelfhealingWeb")
+		return ctrl.Result{}, err
+	}
+
+	// Pod Scailing
+	desiredReplicas := selfhealingWeb.Spec.Replicas
+	currentPods := &corev1.PodList{}
+	if err := r.List(ctx, currentPods, client.InNamespace(req.Namespace), client.MatchingLabels{"app": selfhealingWeb.Name}); err != nil {
+		log.Error(err, "Error Listing Pods")
+		return ctrl.Result{}, err
+	}
+
+	currentReplicas := int32(len(currentPods.Items))
+	if currentReplicas < desiredReplicas {
+		for i := currentReplicas; i < desiredReplicas; i++ {
+			pod := NewPod(&selfhealingWeb)
+			if err := controllerutil.SetControllerReference(&selfhealingWeb, pod, r.Scheme); err != nil {
+				log.Error(err, "Error Setting Controller Reference")
+				return ctrl.Result{}, err
+			}
+			if err := r.Create(ctx, pod); err != nil {
+				log.Error(err, "Error Creating Pod")
+				return ctrl.Result{}, err
+			}
+		}
+	} else if currentReplicas > desiredReplicas {
+		for i := currentReplicas - 1; i >= desiredReplicas; i-- {
+			pod := currentPods.Items[i]
+			if err := r.Delete(ctx, &pod); err != nil {
+				log.Error(err, "Error Deleting Pod")
+				return ctrl.Result{}, err
+			}
+		}
+	}
+	
+	return ctrl.Result{}, nil
+}
+
+// Create Pod
+func NewPod(cr *appv1.SelfhealingWeb) *corev1.Pod {
+	labels := map[string]string{
+		"app": cr.Name,
+	}
+	return &corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			GenerateName: cr.Name + "-",
+			Namespace:    cr.Namespace,
+			Labels:       labels,
+		},
+		Spec: corev1.PodSpec{
+			Containers: []corev1.Container{
+				{
+					Name:  cr.Name,
+					Image: "nginx",
+				},
+			},
+			RestartPolicy: corev1.RestartPolicyAlways,
+		},
+	}
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *SelfhealingWebReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&appv1.SelfhealingWeb{}).
+		Complete(r)
+}
